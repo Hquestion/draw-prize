769359745a793c0a01415220117a2ae80a350966
Hquestion <1035271660@qq.com>
2016-08-27 20:47:10 +0800

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,49 @@
+/**
+ * Created by 贺小雷 on 2016/8/25.
+ */
+var config = {
+    goods: ["谢谢参与", "iPhone 7 Plus", "500元苏果卡", "谢谢参与", "iPad mini4", "50元苏果卡", "谢谢参与 ",
+        "iWatch", "100元苏果卡"],
+    rate: [1,0.0001,0.0005,1,0.001,0.005,1,0.1,0.001],
+    nameList: [{
+        order: '1',
+        name: '熊明娜',
+        prize: 'iPhone 7 Plus',
+        school: '51教育初中部',
+    },{
+        order: '2',
+        name: '赵金',
+        prize: '500元苏果卡',
+        school: '51教育初中部',
+    },{
+        order: '3',
+        name: '张兴智',
+        prize: 'iPhone 7 Plus',
+        school: '51教育初中部',
+    },{
+        order: '4',
+        name: '赵文东',
+        prize: '50元苏果卡',
+        school: '51教育初中部',
+    },{
+        order: '5',
+        name: '王哲',
+        prize: '50元苏果卡',
+        school: '51教育初中部',
+    },{
+        order: '5',
+        name: '王建峰',
+        prize: '100元苏果卡',
+        school: '51教育初中部',
+    },{
+        order: '6',
+        name: '张妍',
+        prize: 'iPad Mini',
+        school: '51教育初中部',
+    },{
+        order: '7',
+        name: '唐全宾',
+        prize: 'iWacth',
+        school: '51教育初中部',
+    }]
+};
